fd3b1040b5718b164bb2cd32121a5e23285ddf28
Peter Beard <peter.b.beard+github@gmail.com>
2013-07-29 18:20:43 -0400

    Prints a message explaining Keith numbers. Also shows steps in a much clearer format.

--- keith.py
+++ keith.py
@@ -10,22 +10,49 @@ def getDigits(value):
 # Check whether the given number is a Keith number
 def isKeith(value, showSteps=False):
 	digits = getDigits(value)
-	values = digits[1:len(digits)]
+	values = digits[0:len(digits)]
 	s = sum(digits)
+	if showSteps:
+		print '  ' + ' + '.join(str(x) for x in values) + ' = ' + str(s)
 	values.append(s)
 	while s < value:
+		values.pop(0)
 		s = sum(values)
 		if showSteps:
-			print 'sum(',values, ') =', s
+			print '  ' + ' + '.join(str(x) for x in values) + ' = ' + str(s)
 		values.append(s)
-		values.pop(0)
 	return s == value
 
+# Explain Keith numbers to the user
+print "A Keith number is any n-digit integer where repeatedly summing groups"
+print "of n numbers from the digits and then the resulting sums that results"
+print "in the original number."
+print
+print "For example, 14 is a Keith number because 1 + 4 = 5, 4 + 5 = 9, and"
+print "5 + 9 = 14."
+print
+print "A Keith cluster is any group of Keith numbers that are multiples of"
+print "the smallest member of the cluster."
+print
+print "This script will search for Keith numbers and Keith clusters up to"
+print "the given upper bound."
+print
+print
 # Get the upper bound for our search
 try:
 	keithMax = int(raw_input('Enter the number to stop searching at: '))
 except:
 	print 'Invalid input.'
+
+try:
+	showSteps = raw_input('Show verification steps (y/n)? ')
+except:
+	print 'Error parsing input.'
+
+if showSteps == 'y':
+	showSteps = True
+else:
+	showSteps = False
 
 # Look for Keith numbers between 10 and keithMax (all single-digit integers are Keith numbers)
 keiths = []
@@ -33,6 +60,9 @@ for k in range(10,keithMax):
 	if isKeith(k):
 		keiths.append(k)
 		print k, 'is a Keith number.'
+		if showSteps:
+			isKeith(k,True)
+			print
 
 # Look for Keith clusters (all cluster members are integer multiples of the smallest member and have the same number of digits)
 clusters = {}
