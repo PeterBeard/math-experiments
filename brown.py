91cb60620088e9b5238c651a8f0098fccc8e11aa
Peter Beard <peter.b.beard+github@gmail.com>
2015-11-28 22:00:39 -0500

    Rewrite for python 3

--- brown.py
+++ brown.py
@@ -1,14 +1,19 @@
-# Guess a number chosen by the user using a clever trick
+"""
+Guess a number chosen by the user using a clever trick
+
+Based on this Numberphile video:
+https://www.youtube.com/watch?v=kQZmZRE0cQY
+"""
 import math
 
 
-# Generate a stack of cards given the indices of the cards and the largest number to be represented
 def generateCards(indices, limit):
+    """Generate a stack of cards given the indices and the largest number"""
     ncards = len(indices)
     indices = sorted(indices)[::-1]
     # The highest number representable with the list is 1111 ... 111, which is the sum of all elements
     if limit > sum(indices):
-        print "Cannot generate enough cards using the given indices."
+        print("Cannot generate enough cards using the given indices.")
         return []
     cards = []
     # Initialize each card to be an empty list
@@ -34,48 +39,51 @@ def generateCards(indices, limit):
     return cards
 
 
-# Display a card in a pleasing way
-def displayCard(card):
+def display_card(card):
+    """Display a card in a pleasing way"""
     nitems = len(card)
-    displaysize = int(math.sqrt(nitems))+1
+    char_width = 6
+    display_size = int(math.sqrt(nitems))+1
+    fill_width = (display_size * char_width) + 1
 
     # Make the top edge of the frame
-    print '+' + '='*((displaysize*3)+1) + '+'
-    for i in range(0, displaysize):
+    print('\u2554' + '\u2550'*fill_width + '\u2557')
+    for i in range(0, display_size):
+        # Empty row
+        print('\u2551' + ' '*fill_width + '\u2551')
         # Left frame edge
-        print '|',
-        for j in range(0, displaysize):
-            index = j+i*displaysize
+        print('\u2551', end='')
+        for j in range(0, display_size):
+            index = j+i*display_size
             # Display the next number
             if index < nitems:
-                print "%2i" % card[index],
+                print("{: ^6n}".format(card[index]), end='')
             # Past the end of the array, print blank spaces
             else:
-                print '  ',
+                print(' '*char_width, end='')
         # Right frame edge
-        print '|'
+        print(' \u2551')
+
     # Bottom frame edge
-    print '+' + '='*((displaysize*3)+1) + '+'
+    print('\u255a' + '\u2550'*fill_width + '\u255d')
 
 
 # Generate cards using the first few Fibonacci numbers and a max value of 30
-maxGuess = 50
-cards = generateCards([1, 2, 3, 5, 8, 13, 21], maxGuess)
+MAX_GUESS = 50
+cards = generateCards([1, 2, 3, 5, 8, 13, 21], MAX_GUESS)
 
-print 'Think of a number between 1 and %i.'
-s = raw_input('Press enter when you\'ve decided on a number.')
-print ''
-print 'Now I\'ll show you some cards and you just have to tell me if you see your number.'
-print ''
+print('Think of a number between 1 and {0}.'.format(MAX_GUESS))
+s = input('Press enter when you\'ve decided on a number.\n')
+print('Now I\'ll show you some cards and you just have to tell me \
+        if you see your number.\n')
 
 
 # The player's number is just the sum of the indices of all of the cards
 # containing that number, i.e. all of its digits using the given sequence.
 s = 0
 for c in cards:
-    displayCard(c)
-    print
-    if raw_input('Is your number on this card [y/n]? ') == 'y':
+    display_card(c)
+    if input('\nIs your number on this card [y/n]? ').lower() == 'y':
         s += c[0]
 
-print 'Your number was', s
+print('\n\n{:^80}\n\n'.format('Your number was {}.'.format(s)))
